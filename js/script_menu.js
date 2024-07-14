5d2811795b0ca9dadc15b4ca14eafea6be94730b
IdrissaG <idrissa.gueye23@protonmail.com>
2024-07-14 17:00:21 +0100

    Update script_menu.js

--- js/script_menu.js
+++ js/script_menu.js
@@ -2,4 +2,11 @@ let btn=document.querySelector('#btn');
     let sidebar = document.querySelector('.sidebar');
     btn.onclick = function(){
         sidebar.classList.toggle('active');
+
+
+
+
+
+
+        
     };
