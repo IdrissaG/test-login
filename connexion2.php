5751630d3b8983223948b8746d527343cf7b57d2
IdrissaG <idrissa.gueye23@protonmail.com>
2024-11-21 16:02:25 +0100

    udt

--- connexion2.php
+++ connexion2.php
@@ -1,3 +1,14 @@
 <?php
-	$connexion = new PDO('mysql:host=localhost;dbname=universite;charset=utf8', 'root', ''); 
-?>
+$servername = "sql311.infinityfree.com"; // Replace with your hosting server's address
+$username = "if0_37756437";       // Replace with your InfinityFree username
+$password = "0OmnmqOHx5bFbaL";      // Replace with your hosting account password
+$dbname = "if0_37756437_universite";  // Replace with your database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
